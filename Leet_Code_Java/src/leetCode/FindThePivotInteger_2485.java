2e3d068c7bb637086a75a27d2fa2e0d1bd114d89
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-03-13 09:02:11 -0400

    Added 2485

---
+++ Leet_Code_Java/src/leetCode/FindThePivotInteger_2485.java
@@ -0,0 +1,20 @@
+package leetCode;
+
+import java.util.stream.IntStream;
+
+public class FindThePivotInteger_2485 {
+    public int pivotInteger(int n) {
+        int tot_sum = (n * (n + 1))/2;
+        int rem_sum = 0;
+
+        for(int num = n; num > 0; num--){
+            if(tot_sum == rem_sum + num)
+                return num;
+
+            tot_sum -= num;
+            rem_sum += num;
+        }
+
+        return -1;
+    }
+}
