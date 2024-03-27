8dd984cc128210d3948f1b0095d9288cf98014d1
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-03-27 12:42:37 -0400

    Added 713

---
+++ Leet_Code_Java/src/leetCode/SubArrayProductLessThanK_713.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+public class SubArrayProductLessThanK_713 {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int res = 0, l = 0, prod = 1;
+
+        for(int r = 0; r < nums.length; r++){
+            prod *= nums[r];
+
+            while(prod >= k && l <= r){
+                prod = prod /nums[l];
+                l++;
+            }
+            res += r - l + 1;
+        }
+        return res;
+    }
+}
