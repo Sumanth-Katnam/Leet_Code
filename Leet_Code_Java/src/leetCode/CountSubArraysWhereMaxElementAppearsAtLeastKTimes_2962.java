c68358f4e20bc4ec939944408625775a53b0f09f
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-03-29 09:41:03 -0400

    Added 2962

---
+++ Leet_Code_Java/src/leetCode/CountSubArraysWhereMaxElementAppearsAtLeastKTimes_2962.java
@@ -0,0 +1,29 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class CountSubArraysWhereMaxElementAppearsAtLeastKTimes_2962 {
+    public long countSubarrays(int[] nums, int k) {
+        long res = 0;
+        int l = 0;
+        int max_ele = 0;
+        for (int num : nums) {
+            max_ele = Math.max(max_ele, num);
+        }
+        int max_cnt = 0;
+
+        for (int num : nums) {
+            if (num == max_ele)
+                max_cnt++;
+
+            while (max_cnt >= k) {
+                if (nums[l] == max_ele)
+                    max_cnt--;
+                l++;
+            }
+            res += l;
+        }
+
+        return res;
+    }
+}
