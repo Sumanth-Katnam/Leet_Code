121d110b0255bbeb7a54615274e89c87b65744c7
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-04-07 14:22:31 -0400

    Updated 678

---
+++ Leet_Code_Java/src/leetCode/Greedy/ValidParenthesesString_678.java
@@ -0,0 +1,27 @@
+package leetCode.Greedy;
+
+public class ValidParenthesesString_678 {
+    public boolean checkValidString(String s) {
+        int maxOpen = 0, minOpen = 0;
+
+        for(char ch: s.toCharArray()){
+            if(ch == '('){
+                maxOpen += 1;
+                minOpen += 1;
+            } else if(ch == ')'){
+                maxOpen -= 1;
+                minOpen -= 1;
+            } else{
+                maxOpen += 1;
+                minOpen -= 1;
+            }
+
+            if(maxOpen < 0)
+                return false;
+
+            if(minOpen < 0)
+                minOpen = 0;
+        }
+        return minOpen == 0;
+    }
+}
