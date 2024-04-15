77684be8ae2af80dd3952b637cc6a2e18d1bb09b
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-04-15 11:18:58 -0400

    Added 129

---
+++ Leet_Code_Java/src/leetCode/Trees/SumRootToLeafNumbers_129.java
@@ -0,0 +1,23 @@
+package leetCode.Trees;
+
+import helpers.TreeNodeMain.TreeNode;
+
+public class SumRootToLeafNumbers_129 {
+    private int res = 0;
+    private void dfs(TreeNode node, int num){
+        num = num * 10 + node.val;
+        if(node.left == null && node.right == null){
+            res += num;
+        } else {
+            if(node.left != null)
+                dfs(node.left, num);
+            if(node.right != null)
+                dfs(node.right, num);
+        }
+    }
+
+    public int sumNumbers(TreeNode root) {
+        dfs(root, 0);
+        return res;
+    }
+}
