c2db6cff6faf2e80ad27b5f32d754adb1aed683a
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-04-26 10:55:58 -0400

    Added 1289

---
+++ Leet_Code_Java/src/leetCode/DP2/MinimumFallingPathSumII_1289.java
@@ -0,0 +1,28 @@
+package leetCode.DP2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MinimumFallingPathSumII_1289 {
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length;
+        int[] prev_row = grid[0];
+
+        int temp;
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n; j++){
+                temp = Integer.MAX_VALUE;
+                for(int k = 0; k < n; k++){
+                    if(j != k){
+                        temp = Math.min(temp, grid[i][j] + prev_row[k]);
+                    }
+                }
+                grid[i][j] = temp;
+            }
+            prev_row = grid[i];
+        }
+        return Arrays.stream(prev_row).min().getAsInt();
+    }
+}
