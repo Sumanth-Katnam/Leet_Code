609d5e9128d718ce7dd04cacb72a1b7040b02e8e
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-05-04 19:51:37 -0400

    Added 443

---
+++ Leet_Code_Java/src/leetCode/TwoPointers/Medium/StringCompression_443.java
@@ -0,0 +1,27 @@
+package leetCode.TwoPointers.Medium;
+
+public class StringCompression_443 {
+    public int compress(char[] chars) {
+        int ans = 0;
+
+        for (int i = 0; i < chars.length;) {
+            char letter = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == letter) {
+                count++;
+                i++;
+            }
+
+            chars[ans++] = letter;
+
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[ans++] = c;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
