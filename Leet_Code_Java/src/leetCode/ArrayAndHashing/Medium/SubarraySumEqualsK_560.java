4a053f98997284789324483b69a4b2ba8cf2e142
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-05-06 14:49:03 -0400

    Added 560

---
+++ Leet_Code_Java/src/leetCode/ArrayAndHashing/Medium/SubarraySumEqualsK_560.java
@@ -0,0 +1,20 @@
+package leetCode.ArrayAndHashing.Medium;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK_560 {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0;
+        int ans = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return ans;
+    }
+}
