13261f94ff5d820c156ac9794c1ca9c95b1468eb
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-05-09 10:36:27 -0400

    Updated 3075

--- Leet_Code_Java/src/leetCode/Greedy/Medium/MaximizeHappinessOfSelectedChildren_3075.java
+++ Leet_Code_Java/src/leetCode/Greedy/Medium/MaximizeHappinessOfSelectedChildren_3075.java
@@ -8,10 +8,11 @@ public class MaximizeHappinessOfSelectedChildren_3075 {
         Arrays.sort(happiness);
         long res = 0;
         int n = happiness.length, j = 0;
-
+        int selectedVal;
         for (int i = n - 1; i >= n - k; --i) {
-            happiness[i] = Math.max(happiness[i] - j++, 0);
-            res += happiness[i];
+            selectedVal = happiness[i] - j++;
+            if(selectedVal > 0)
+                res += selectedVal;
         }
 
         return res;
