0144284496f5ee2d038608e03f06d975d043e0f4
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2024-06-03 10:02:25 -0400

    Added 2486

---
+++ Leet_Code_Java/src/leetCode/ArrayAndHashing/Medium/AppendCharactersToStringToMakeSubsequence_2486.java
@@ -0,0 +1,13 @@
+package leetCode.ArrayAndHashing.Medium;
+
+public class AppendCharactersToStringToMakeSubsequence_2486 {
+    public int appendCharacters(String s, String t) {
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j))
+                j++;
+            i++;
+        }
+        return t.length() - j;
+    }
+}
